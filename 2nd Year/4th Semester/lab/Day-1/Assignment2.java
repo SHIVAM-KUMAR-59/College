58c4a5d9907f4254643179911bbf15ebddac3fad
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-12-07 23:55:34 +0530

    06-12-2024

--- 2nd Year/4th Semester/lab/Day-1/Assignment2.java
+++ 2nd Year/4th Semester/lab/Day-1/Assignment2.java
@@ -5,7 +5,7 @@
 public class Assignment2 {
     public static void main(String[] args) {
         System.out.println("Prime number between 1 to 1000 are");
-        for(int i = 1; i <= 500; i++){
+        for(int i = 1; i < 500; i++){
             int flag = 0;
             for(int j = 2; j < i; j++){
                 if(i % j == 0){
