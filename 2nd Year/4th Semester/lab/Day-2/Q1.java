0bdbacf51b30948c9c9b0d82f36b6eb3d9656908
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-12-14 12:42:34 +0530

    13-12-2024

---
+++ 2nd Year/4th Semester/lab/Day-2/Q1.java
@@ -0,0 +1,24 @@
+/*
+ * WAP to find the maximum of 3 numbers take input through command linke prompt
+ */
+
+public class Q1{
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        System.out.println(max);
+    }
+}
+
+/*
+ * $ javac Q1.java
+ * $ java Q1 1 2 3
+ */
---
+++ 2nd Year/4th Semester/lab/Day-2/Q2.java
@@ -0,0 +1,27 @@
+/*
+ * WAP to print all prime numbers between a user input range, the range should be entered through command line prompt.
+ */
+
+ public class Q2{
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        for (int i = a; i <= b; i++) {
+            int count = 0;
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0) {
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
+
+/*
+ * $ javac Q2.java
+ * $ java Q2 1 10
+ */
