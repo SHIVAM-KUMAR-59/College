cf5b396a9d0510178814a3d35bf77873ca1604cd
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-01-16 21:09:54 +0530

    18-12-2024

--- 2nd Year/4th Semester/Operating Systems Lab/Day-1/q4.sh
+++ 2nd Year/4th Semester/Operating Systems Lab/Day-1/q4.sh
@@ -2,9 +2,14 @@
 
 #!/bin/bash
 date="Dec 18 2024"
-split = $(echo $date | cut -d ' ' -f2,3,4)
-day = $(echo $split | cut -d ' ' -f1)
-month = $(echo $split | cut -d ' ' -f2)
-year = $(echo $split | cut -d ' ' -f3)
-new_date = $day/$month/$year
+day=$(echo $date | cut -d ' ' -f2)        # Extract the day
+month=$(echo $date | cut -d ' ' -f1)      # Extract the month
+year=$(echo $date | cut -d ' ' -f3)       # Extract the year
+
+# Convert the month name to its numeric representation
+month_num=$(date -d "1 $month" "+%m")
+
+# Format the new date
+new_date="$day/$month_num/$year"
+
 echo $new_date
