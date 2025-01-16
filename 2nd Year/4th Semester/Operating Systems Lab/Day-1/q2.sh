70ba1749401a3d15907fc96ef8d79ba9e4e7d236
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-01-16 21:02:45 +0530

    18-12-2024

---
+++ 2nd Year/4th Semester/Operating Systems Lab/Day-1/q2.sh
@@ -0,0 +1,6 @@
+# Remove a directory which may/may not be empty
+
+#!/bin/bash
+rm -rf directory_name # Remove the directory and all its contents
+
+rmdir directory_name # Remove the directory only if it is empty
