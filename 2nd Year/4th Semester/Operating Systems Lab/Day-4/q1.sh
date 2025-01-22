83bb3fc451497a2d7977297575ab82291708710f
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-01-22 12:26:18 +0530

    22-01-2025

--- 2nd Year/4th Semester/Operating Systems Lab/Day-4/q1.sh
+++ 2nd Year/4th Semester/Operating Systems Lab/Day-4/q1.sh
@@ -1,2 +1,13 @@
+# WAP to show the usability of fprintf, fscanf, write, read, system and sprintf, strtok function
+
 #!/bin/bash
-echo "Hello"
+for i in {1..5}
+do
+    for j in {1..5}
+    do
+        tput cup $i $j
+        echo "ABC"
+        tput cup clear
+        sleep 0.75
+    done
+done
---
+++ 2nd Year/4th Semester/Operating Systems Lab/Day-4/q2.c
@@ -0,0 +1,20 @@
+// WAP to create n child process, each process should display its own ID and parent ID
+#include<stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <sys/wait.h>
+int main(){
+    int n;
+    printf("Enter the number of child process: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        int pid=fork();
+        if(pid==0){
+            printf("Child process ID: %d\n",getpid());
+            printf("Parent process ID: %d\n",getppid());
+            exit(0);
+        }
+    }
+    printf("Parent process ID: %d\n",getpid());
+    return 0;
+}
