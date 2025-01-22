f731b540aee883923ed7000d17256c4f35049364
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-01-22 13:39:24 +0530

    22-01-2025

--- 2nd Year/4th Semester/Operating Systems Lab/Day-4/q4.c
+++ 2nd Year/4th Semester/Operating Systems Lab/Day-4/q4.c
@@ -6,15 +6,15 @@
 #include <sys/wait.h>
 
 int main(){
-    int pid = fork();
-    if(pid == 0){
+    int t = fork();
+    if(t == 0){
         printf("Child process ID: %d\n",getpid());
         printf("Parent process ID: %d\n",getppid());
         exit(0);
     }else{
         printf("Parent process ID: %d\n",getpid());
         wait(NULL);
-        printf("Zombie process ID: %d\n",pid);
+        printf("Zombie process ID: %d\n",t);
     }
     return 0;
 }
