770c5d470515d28356ffe10c4ea3dbe654f705d7
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-01-23 22:08:23 +0530

    22-01-2025

--- 2nd Year/4th Semester/Operating Systems Lab/Day-4/q1.c
+++ 2nd Year/4th Semester/Operating Systems Lab/Day-4/q1.c
@@ -1,4 +1,4 @@
-// WAP to show the usability of fprintf, fscanf, write, read, system and sprintf, strtok function
+// WAP to show the usability of fprintf, fscanf, write, read, system and sprintf, strtok function 
 
 #include <stdio.h>
 #include <stdlib.h>
