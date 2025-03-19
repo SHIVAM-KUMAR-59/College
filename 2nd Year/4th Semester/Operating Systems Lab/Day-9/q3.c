9f592d12504bf6776d56714bf9754e9c82e13d6b
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-03-19 13:27:55 +0530

    19-03-2025

--- 2nd Year/4th Semester/Operating Systems Lab/Day-9/q3.c
+++ 2nd Year/4th Semester/Operating Systems Lab/Day-9/q3.c
@@ -1,28 +1,29 @@
-
 #include <stdio.h>
 #include <pthread.h>
 
-void *fun(void *arg){
-    pthread_t t;
+void *fun(void *arg) {
     int a = *((int *)arg);
-    int b = a / 2;
-    printf("Value in fun: %d\n", a);
-    if(b > 0){
-        for(int i = 0; i < b; i++){
-            pthread_create(&t, NULL, fun, &b);
-            pthread_join(t, NULL);
-        }
-        
+
+    for (int i = 0; i < 8 / a; i++) {
+        printf("%d\n", a);
     }
-    
+
+    if (a > 1) {
+        pthread_t t;
+        int b = a / 2;
+        pthread_create(&t, NULL, fun, &b);
+        pthread_join(t, NULL); 
+    }
+
     return NULL;
 }
 
-int main(){
-
-    pthread_t  t;
+int main() {
+    pthread_t t;
     int a = 8;
+
     pthread_create(&t, NULL, fun, &a);
     pthread_join(t, NULL);
+
     return 0;
 }
---
+++ 2nd Year/4th Semester/Operating Systems Lab/Day-9/q4.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+struct node {
+    int value;
+    struct node *left;
+    struct node *right;
+};
+
+void *fun(void *arg) {
+    int a = *((int *)arg);
+
+    struct node *node = (struct node *)malloc(sizeof(struct node));
+    node->value = a;
+    node->left = NULL;
+    node->right = NULL;
+
+    if (a > 1) {
+        pthread_t left, right;
+        int leftVal = a / 2;
+        int rightVal = a / 2;
+
+        struct node *leftAns;
+        pthread_create(&left, NULL, fun, &leftVal);
+        pthread_join(left, (void **)&leftAns);
+        node->left = leftAns;
+
+        struct node *rightAns;
+        pthread_create(&right, NULL, fun, &rightVal);
+        pthread_join(right, (void **)&rightAns);
+        node->right = rightAns;
+    }
+
+    return node;
+}
+
+
+void postOrder(struct node *root) {
+    if (root == NULL) {
+        return;
+    }
+
+    postOrder(root->left);
+    postOrder(root->right);
+    printf("%d ", root->value);
+}
+
+
+int main() {
+    pthread_t t;
+    int a = 8;
+    struct node *root = NULL;
+
+    pthread_create(&t, NULL, fun, &a);
+    pthread_join(t, (void **)&root);
+
+    printf("\nPost-order Traversal: ");
+    postOrder(root);
+    printf("\n");
+
+    return 0;
+}
