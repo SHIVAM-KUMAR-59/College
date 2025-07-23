ce509adf0d6fd4a327458b52e96ad0650890bc1d
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-07-23 19:51:01 +0530

    22-07-2025

--- 3rd Year/5th Semester/Computer Networking Lab/Day-01/3.c
+++ 3rd Year/5th Semester/Computer Networking Lab/Day-01/3.c
@@ -1,14 +1,23 @@
 // WAP to extract each byte from a given number and store them in separate character variable and print content of those variables
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 
 int main() {
-    int a;
-    printf("Enter the number:\n");
-    scanf("%d", &a);
-    char arr[256] = (bytes)(a);
-    printf("%s", arr);
-    
+    unsigned int num;
+    char byte1, byte2, byte3, byte4;
+
+    printf("Enter an unsigned integer: ");
+    scanf("%u", &num);
+
+    byte1 = (num & 0x000000FF);
+    byte2 = (num & 0x0000FF00) >> 8;
+    byte3 = (num & 0x00FF0000) >> 16;
+    byte4 = (num & 0xFF000000) >> 24;
+
+    printf("Byte 1: 0x%02X\n", (unsigned char)byte1);
+    printf("Byte 2: 0x%02X\n", (unsigned char)byte2);
+    printf("Byte 3: 0x%02X\n", (unsigned char)byte3);
+    printf("Byte 4: 0x%02X\n", (unsigned char)byte4);
+
     return 0;
 }
