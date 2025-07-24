a7d46dbda5adea8eaa964eb1352816330d27d6a2
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-07-24 09:59:40 +0530

    24-07-2025

--- 3rd Year/5th Semester/Design and Analysis of Algorith Lab/Day-01/Q4.java
+++ 3rd Year/5th Semester/Design and Analysis of Algorith Lab/Day-01/Q4.java
@@ -6,59 +6,42 @@ import java.util.*;
 
 public class Q4 {
 
-    // Function to count duplicate elements
-    private static int totalDuplicateElement(int[] arr) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int count = 0;
+    private static void analyzeDuplicates(int[] arr) {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        int duplicateCount = 0;
+        int mostFreq = 0;
+        int mostRepeated = arr[0];
 
-        for (int val : arr) {
-            map.put(val, map.getOrDefault(val, 0) + 1);
-        }
+        for (int num : arr) {
+            int freq = freqMap.getOrDefault(num, 0) + 1;
+            freqMap.put(num, freq);
 
-        for (int freq : map.values()) {
-            if (freq > 1) {
-                count++;
+            if (freq == 2) {
+                duplicateCount++; 
+            }
+
+            if (freq > mostFreq) {
+                mostFreq = freq;
+                mostRepeated = num;
             }
         }
 
-        return count;
-    }
-
-    // Function to find the most repeating element
-    private static int mostRepeatingElement(int[] arr) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int maxFreq = 0;
-        int maxVal = arr[0];
-
-        for (int val : arr) {
-            map.put(val, map.getOrDefault(val, 0) + 1);
-            if (map.get(val) > maxFreq) {
-                maxFreq = map.get(val);
-                maxVal = val;
-            }
-        }
-
-        return maxVal;
+        System.out.println("Total number of duplicate elements: " + duplicateCount);
+        System.out.println("Most repeating element: " + mostRepeated + " (repeated " + mostFreq + " times)");
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter size of array: ");
         int n = sc.nextInt();
-
         int[] arr = new int[n];
+
         System.out.println("Enter elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        int totalDuplicates = totalDuplicateElement(arr);
-        int mostFrequent = mostRepeatingElement(arr);
-
-        System.out.println("Total number of duplicate elements: " + totalDuplicates);
-        System.out.println("Most repeating element: " + mostFrequent);
-
+        analyzeDuplicates(arr);
         sc.close();
     }
 }
