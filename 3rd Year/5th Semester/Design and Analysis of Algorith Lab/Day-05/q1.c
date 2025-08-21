e69e4af6df8af3a34458e659b115617d03adc43b
SHIVAM_KUMAR_59 <shivamkumardev01@gmail.com>
2025-08-21 04:39:42 +0000

    21-08-2025

--- 3rd Year/5th Semester/Design and Analysis of Algorith Lab/Day-05/q1.c
+++ 3rd Year/5th Semester/Design and Analysis of Algorith Lab/Day-05/q1.c
@@ -1,54 +1,57 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void printArray(int arr[], int n){
-	for(int i = 0; i < n; i++){
-		printf("%d ", arr[i]);
-	}
-	printf("/n");
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
 
-void heapify(int arr[], int n, int i){
-	int largest = i;
-	int left = 2 * i + 1;
-	int right = 2 * i + 2;
-	
-	if (left < n && arr[left] > arr[largest])
+void heapify(int arr[], int n, int i) {
+    int largest = i;         
+    int left = 2 * i + 1;    
+    int right = 2 * i + 2;   
+
+    if (left < n && arr[left] > arr[largest])
         largest = left;
 
     if (right < n && arr[right] > arr[largest])
         largest = right;
 
     if (largest != i) {
-    	int temp = arr[i];
-    	arr[i] = arr[largest];
-    	arr[largest] = temp;
+        swap(&arr[i], &arr[largest]);
         heapify(arr, n, largest);
     }
 }
 
-void heapSort(int arr[], int n){
-	for(int i = n - 1; i >= 0; i--){
-		int temp = arr[0];
-		arr[0] = arr[i];
-		arr[i] = temp;
-		
-		heapify(arr, i, 0);
-	}
+void heapSort(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i = n - 1; i > 0; i--) {
+        swap(&arr[0], &arr[i]);     
+        heapify(arr, i, 0);        
+    }
 }
 
-int main(){
-	int arr[10];
-	printf("Enter 10 elements in the array:\n");
-	for(int i = 0; i < 10; i++){
-		scanf("%d", &arr[i]);
-	}
-	
-	printf("Entered array: \n");
-	printArray(arr, 10);
-	
-	heapSort(arr, 10);
-	printf("Sorted array:\n");
-	printArray(arr, 10);
-	
-	return 0;
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
 }
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    heapSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
+
